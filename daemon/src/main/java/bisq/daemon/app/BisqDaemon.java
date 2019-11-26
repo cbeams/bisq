72a458a310cbeaced1c5d72e9ba2259597ecb5df
Chris Beams <chris@beams.io>
2019-11-26 19:37:19 +0100

    Eliminate remaining 'HeadlessApp' abstractions
    Problem: BisqDaemon is the only implementation of a 'headless app' as
    envisioned by the HeadlessApp and BisqHeadlessApp abstractions. These
    types do not create any benefit in the form of code reuse and they end
    up creating confusion when one is trying to understand what the Bisq
    daemon is and does.
    
    Solution: This commit pushes functionality in BisqHeadlessApp down into
    BisqDaemonMain and removes the BisqHeadlessApp class and HeadlessApp
    interface.

--- daemon/src/main/java/bisq/daemon/app/BisqDaemon.java
+++ daemon/src/main/java/bisq/daemon/app/BisqDaemon.java
@@ -17,7 +17,126 @@
 
 package bisq.daemon.app;
 
-import bisq.core.app.BisqHeadlessApp;
+import bisq.core.app.BisqSetup;
+import bisq.core.trade.TradeManager;
 
-public class BisqDaemon extends BisqHeadlessApp {
+import bisq.common.UserThread;
+import bisq.common.setup.GracefulShutDownHandler;
+import bisq.common.setup.UncaughtExceptionHandler;
+import bisq.common.storage.CorruptedDatabaseFilesHandler;
+import bisq.common.util.Profiler;
+
+import com.google.inject.Injector;
+
+import java.util.concurrent.TimeUnit;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class BisqDaemon implements UncaughtExceptionHandler, BisqSetup.BisqSetupListener {
+    private static final long LOG_MEMORY_PERIOD_MIN = 10;
+    @Getter
+    private static Runnable shutDownHandler;
+
+    @Setter
+    protected Injector injector;
+    @Setter
+    private GracefulShutDownHandler gracefulShutDownHandler;
+    private boolean shutDownRequested;
+    protected BisqSetup bisqSetup;
+    private CorruptedDatabaseFilesHandler corruptedDatabaseFilesHandler;
+    private TradeManager tradeManager;
+
+    public BisqDaemon() {
+        shutDownHandler = this::stop;
+    }
+
+    public void startApplication() {
+        try {
+            bisqSetup = injector.getInstance(BisqSetup.class);
+            bisqSetup.addBisqSetupListener(this);
+
+            corruptedDatabaseFilesHandler = injector.getInstance(CorruptedDatabaseFilesHandler.class);
+            tradeManager = injector.getInstance(TradeManager.class);
+
+            setupHandlers();
+
+            UserThread.runPeriodically(() -> Profiler.printSystemLoad(log), LOG_MEMORY_PERIOD_MIN, TimeUnit.MINUTES);
+        } catch (Throwable throwable) {
+            log.error("Error during app init", throwable);
+            handleUncaughtException(throwable, false);
+        }
+    }
+
+    @Override
+    public void onSetupComplete() {
+        log.info("onSetupComplete");
+    }
+
+    protected void setupHandlers() {
+        bisqSetup.setDisplayTacHandler(acceptedHandler -> {
+            log.info("onDisplayTacHandler: We accept the tacs automatically in headless mode");
+            acceptedHandler.run();
+        });
+        bisqSetup.setCryptoSetupFailedHandler(msg -> log.error("onCryptoSetupFailedHandler: msg={}", msg));
+        bisqSetup.setDisplayTorNetworkSettingsHandler(show -> log.info("onDisplayTorNetworkSettingsHandler: show={}", show));
+        bisqSetup.setSpvFileCorruptedHandler(msg -> log.error("onSpvFileCorruptedHandler: msg={}", msg));
+        bisqSetup.setChainFileLockedExceptionHandler(msg -> log.error("onChainFileLockedExceptionHandler: msg={}", msg));
+        bisqSetup.setLockedUpFundsHandler(msg -> log.info("onLockedUpFundsHandler: msg={}", msg));
+        bisqSetup.setShowFirstPopupIfResyncSPVRequestedHandler(() -> log.info("onShowFirstPopupIfResyncSPVRequestedHandler"));
+        bisqSetup.setRequestWalletPasswordHandler(aesKeyHandler -> log.info("onRequestWalletPasswordHandler"));
+        bisqSetup.setDisplayUpdateHandler((alert, key) -> log.info("onDisplayUpdateHandler"));
+        bisqSetup.setDisplayAlertHandler(alert -> log.info("onDisplayAlertHandler. alert={}", alert));
+        bisqSetup.setDisplayPrivateNotificationHandler(privateNotification -> log.info("onDisplayPrivateNotificationHandler. privateNotification={}", privateNotification));
+        bisqSetup.setDaoErrorMessageHandler(errorMessage -> log.error("onDaoErrorMessageHandler. errorMessage={}", errorMessage));
+        bisqSetup.setDaoWarnMessageHandler(warnMessage -> log.warn("onDaoWarnMessageHandler. warnMessage={}", warnMessage));
+        bisqSetup.setDisplaySecurityRecommendationHandler(key -> log.info("onDisplaySecurityRecommendationHandler"));
+        bisqSetup.setDisplayLocalhostHandler(key -> log.info("onDisplayLocalhostHandler"));
+        bisqSetup.setWrongOSArchitectureHandler(msg -> log.error("onWrongOSArchitectureHandler. msg={}", msg));
+        bisqSetup.setVoteResultExceptionHandler(voteResultException -> log.warn("voteResultException={}", voteResultException.toString()));
+        bisqSetup.setRejectedTxErrorMessageHandler(errorMessage -> log.warn("setRejectedTxErrorMessageHandler. errorMessage={}", errorMessage));
+
+        //TODO move to bisqSetup
+        corruptedDatabaseFilesHandler.getCorruptedDatabaseFiles().ifPresent(files -> log.warn("getCorruptedDatabaseFiles. files={}", files));
+        tradeManager.setTakeOfferRequestErrorMessageHandler(errorMessage -> log.error("onTakeOfferRequestErrorMessageHandler"));
+    }
+
+    public void stop() {
+        if (!shutDownRequested) {
+            UserThread.runAfter(() -> {
+                gracefulShutDownHandler.gracefulShutDown(() -> {
+                    log.debug("App shutdown complete");
+                });
+            }, 200, TimeUnit.MILLISECONDS);
+            shutDownRequested = true;
+        }
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // UncaughtExceptionHandler implementation
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public void handleUncaughtException(Throwable throwable, boolean doShutDown) {
+        if (!shutDownRequested) {
+            try {
+                try {
+                    log.error(throwable.getMessage());
+                } catch (Throwable throwable3) {
+                    log.error("Error at displaying Throwable.");
+                    throwable3.printStackTrace();
+                }
+                if (doShutDown)
+                    stop();
+            } catch (Throwable throwable2) {
+                // If printStackTrace cause a further exception we don't pass the throwable to the Popup.
+                log.error(throwable2.toString());
+                if (doShutDown)
+                    stop();
+            }
+        }
+    }
 }
